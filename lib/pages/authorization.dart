1909897bc1bfa33d8566a73b3da938bb70ebff50
olardaniil <olardaniil@yandex.ru>
2023-02-21 16:12:36 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lib/pages/authorization.dart

--- lib/pages/authorization.dart
+++ lib/pages/authorization.dart
@@ -1,4 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+
+import '../generated/locale_keys.g.dart';
+// import 'package:vkurse_app/generated/locale_keys.g.dart';
+
+// import 'generated/locale_keys.g.dart';
 
 // Основной класс для отображения страницы авторизации - ввод номера
 class AuthProvider extends StatelessWidget {
@@ -13,12 +19,25 @@ class AuthProvider extends StatelessWidget {
         title: const Text('Авторизация', ),
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 146, 6, 228),
+        leading: IconButton(
+          icon: Icon(Icons.language), 
+          onPressed: () {
+            if (context.locale == Locale('ru')) {
+              context.setLocale(Locale('en'));
+            } else {
+              context.setLocale(Locale('ru'));
+            }
+            print(LocaleKeys.Authorisation.tr());
+          },
+        ),
       ),
 
-      body: const Center(
-        child: 
-          Text('Авторизация', style: TextStyle(color: Colors.white),),
-          
+      body: Center(
+        child:
+          Text(
+            LocaleKeys.Authorisation.tr(),
+            style: TextStyle(color: Colors.white),
+          ),
       ),
     );
   }
