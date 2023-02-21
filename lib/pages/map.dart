1db09ec31c090b1f2444ac2ff419808a574c93ac
olardaniil <olardaniil@yandex.ru>
2023-02-21 13:29:59 +0300

    modified:   lib/pages/map.dart

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Map extends StatefulWidget {
-  const Map({Key, key}) : super(key: key);
+  const Map({Key? key}) : super(key: key);
 
   @override
   MapState createState() => MapState();
