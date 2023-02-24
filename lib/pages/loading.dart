8818a99604b9141e75648c7d0bc9cadb6349a327
FomichevND <nikitafomichev2003@mail.ru>
2023-02-24 23:28:22 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   assets/images/loading.gif
    	new file:   lib/pages/loading.dart
    	modified:   lib/ui/main_navigation.dart
    	modified:   pubspec.yaml

---
+++ lib/pages/loading.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/loading.gif'),
+                  radius: 200,
+                  backgroundColor: Colors.white,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
