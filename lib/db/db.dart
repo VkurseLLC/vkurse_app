46c09b3a195e183352105e96508b33afe0ffd071
seemyown <semyon.albeev@gmail.com>
2023-02-24 23:42:43 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is behind 'origin/master' by 3 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   lib/db/db.dart

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -1,25 +1,25 @@
 import 'package:mysql_client/mysql_client.dart';
 
 Future main() async {
-  print("Connection to mySql server...");
+  print("Connection to mySql ...");
 
-  //connection
+  // creating connection conditions
   final conn = await MySQLConnection.createConnection(
       host: "95.163.241.100",
       port: 3306,
       userName: "super_user",
       password: "****9963AAdd",
       databaseName: "vkurse_database");
-
+  // create connect
   await conn.connect();
   print("Connected");
-
+  // make request
   var result = await conn.execute(
       "SELECT phone_number, verification_code FROM phone_number_verification_codes");
-
+  //print results
   for (final row in result.rows) {
     print(row.assoc());
   }
-
+  // close connections
   await conn.close();
 }
