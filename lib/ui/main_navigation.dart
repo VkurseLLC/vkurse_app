7f90880cb5ebefb7380901ccbe322b66e83e9411
FomichevND <nikitafomichev2003@mail.ru>
2023-03-05 21:26:09 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lib/ui/main_navigation.dart
    
    Changes not staged for commit:
    	modified:   lib/pages/auth_verification_code.dart
    	modified:   linux/flutter/generated_plugin_registrant.cc
    	modified:   linux/flutter/generated_plugin_registrant.h
    	modified:   linux/flutter/generated_plugins.cmake
    	modified:   macos/Flutter/GeneratedPluginRegistrant.swift
    	modified:   windows/flutter/generated_plugin_registrant.cc
    	modified:   windows/flutter/generated_plugin_registrant.h
    	modified:   windows/flutter/generated_plugins.cmake

--- lib/ui/main_navigation.dart
+++ lib/ui/main_navigation.dart
@@ -11,29 +11,25 @@ import 'package:vkurse_app/pages/auth_phone_number.dart';
 import 'package:vkurse_app/pages/loading.dart';
 import 'package:vkurse_app/pages/input_username.dart';
 
-
 abstract class MainNavigationRoutNames {
   static const auth = '/auth';
   static const map = '/map';
 }
-
 
 class MainNavigation {
   String initialRoute(bool isAuth) =>
       isAuth ? MainNavigationRoutNames.map : MainNavigationRoutNames.auth;
 
   final routes = <String, Widget Function(BuildContext)>{
-    // Если нужно затестить свой экран, то разкомитьте следующую строку и вместо > AuthProvider < вставьте нужный класс
-    // '/auth': (context) => const ProfileFilling(),
-
     // Написанное ниже не трогать !!!
     '/auth': (context) => const AuthProvider(),
     '/auth_get_phone_number': (context) => const GetPhoneNumber(),
     '/auth_get_verification_code': (context) => const AuthGetVerificationCode(),
     '/auth_post_verification_code': (context) => const AuthVerificationCode(),
-    
+
+    // Если нужно затестить свой экран, то разкомитьте следующую строку и вместо > AuthProvider < вставьте нужный класс
+    //'/auth': (context) => const AuthVerificationCode(),
+
     '/map': (context) => const Map(),
-
-
   };
 }
