4e219edff142f40d5a321847edbb13c58e00714c
THKsss <pavukdmitriy@gmail.com>
2023-03-07 02:19:45 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lib/ui/main_navigation.dart
    
    Changes not staged for commit:
    	modified:   linux/flutter/generated_plugin_registrant.cc
    	modified:   linux/flutter/generated_plugin_registrant.h
    	modified:   linux/flutter/generated_plugins.cmake
    	modified:   macos/Flutter/GeneratedPluginRegistrant.swift
    	modified:   pubspec.lock
    	modified:   windows/flutter/generated_plugin_registrant.cc
    	modified:   windows/flutter/generated_plugin_registrant.h
    	modified:   windows/flutter/generated_plugins.cmake

--- lib/ui/main_navigation.dart
+++ lib/ui/main_navigation.dart
@@ -4,7 +4,10 @@ import 'package:vkurse_app/pages/auth_verification_code.dart';
 import 'package:vkurse_app/pages/profile_filling.dart';
 import 'package:vkurse_app/pages/auth_get_verification_code.dart';
 import 'package:vkurse_app/pages/map.dart';
+<<<<<<< HEAD
 import 'package:vkurse_app/pages/test.dart';
+=======
+>>>>>>> 46c80e90c9d8874d49372b77e8c5ddc536e6b135
 import 'package:vkurse_app/pages/end_profile_filling.dart';
 
 import 'package:vkurse_app/pages/welcome.dart';
@@ -29,7 +32,11 @@ class MainNavigation {
     '/auth_post_verification_code': (context) => const AuthVerificationCode(),
 
     // Если нужно затестить свой экран, то разкомитьте следующую строку и вместо > AuthProvider < вставьте нужный класс
+<<<<<<< HEAD
     // '/auth': (context) => ProfileFilling(),
+=======
+    '/auth': (context) => const AuthVerificationCode(),
+>>>>>>> 46c80e90c9d8874d49372b77e8c5ddc536e6b135
 
     '/map': (context) => const Map(),
   };
