133d67a15d357e35eb560367203db2185bcd4d27
olardaniil <olardaniil@yandex.ru>
2023-03-11 01:11:27 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lib/data/api_account_data.dart
    	new file:   lib/pages/profile.dart
    	modified:   lib/pages/test.dart
    	modified:   lib/ui/main_navigation.dart

--- lib/ui/main_navigation.dart
+++ lib/ui/main_navigation.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+
+import 'package:vkurse_app/pages/auth_get_verification_code.dart';
+import 'package:vkurse_app/pages/auth_phone_number.dart';
 import 'package:vkurse_app/pages/auth_phone_number.dart';
 import 'package:vkurse_app/pages/auth_verification_code.dart';
-import 'package:vkurse_app/pages/profile_filling.dart';
-import 'package:vkurse_app/pages/auth_get_verification_code.dart';
-import 'package:vkurse_app/pages/map.dart';
-import 'package:vkurse_app/pages/test.dart';
 import 'package:vkurse_app/pages/end_profile_filling.dart';
-
-import 'package:vkurse_app/pages/welcome.dart';
-import 'package:vkurse_app/pages/auth_phone_number.dart';
-import 'package:vkurse_app/pages/loading.dart';
 import 'package:vkurse_app/pages/input_username.dart';
+import 'package:vkurse_app/pages/loading.dart';
+import 'package:vkurse_app/pages/map.dart';
+import 'package:vkurse_app/pages/profile.dart';
+import 'package:vkurse_app/pages/profile_filling.dart';
+import 'package:vkurse_app/pages/welcome.dart';
+
+import 'package:vkurse_app/pages/test.dart';
 
 abstract class MainNavigationRoutNames {
   static const auth = '/auth';
@@ -31,9 +33,11 @@ class MainNavigation {
     '/initial_setting_username': (context) => const InputUsername(),
     '/initial_setting_accaunt_data': (context) => const ProfileFilling(),
 
+    '/profile': (context) => const Profile(),
+
     '/map': (context) => const Map(),
 
     // Если нужно затестить свой экран, то разкомитьте следующую строку и вместо > AuthProvider < вставьте нужный класс
-    '/auth': (context) => InputUsername(),
+    // '/auth': (context) => Profile(),
   };
 }
