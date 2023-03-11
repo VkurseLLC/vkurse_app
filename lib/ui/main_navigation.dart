4cae712f4ab2b698f554ec784a26d203c2901698
FomichevND <nikitafomichev2003@mail.ru>
2023-03-11 21:25:03 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lib/pages/test.dart
    	modified:   lib/ui/main_navigation.dart
    	modified:   pubspec.yaml

--- lib/ui/main_navigation.dart
+++ lib/ui/main_navigation.dart
@@ -34,6 +34,6 @@ class MainNavigation {
     '/map': (context) => const Map(),
 
     // Если нужно затестить свой экран, то разкомитьте следующую строку и вместо > AuthProvider < вставьте нужный класс
-    '/auth': (context) => AuthVerificationCode(),
+    '/auth': (context) => Profile(),
   };
 }
