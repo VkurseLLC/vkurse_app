b7da2deb3f2ecede7b0ebd7dbc54092a26b87075
THKsss <pavukdmitriy@gmail.com>
2023-03-16 00:19:14 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lib/pages/map.dart
    
    Changes not staged for commit:
    	modified:   lib/pages/test.dart
    	modified:   lib/ui/main_navigation.dart
    	modified:   pubspec.lock
    	modified:   pubspec.yaml

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,17 +1,60 @@
-// ignore_for_file: unnecessary_new
-import 'dart:math';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart' as latLng;
 
+import 'package:vkurse_app/pages/app_location.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
+import 'package:geolocator/geolocator.dart';
+
 class Map extends StatefulWidget {
+
   @override
   _Map createState() => _Map();
+
 }
+
 class _Map extends State<Map> {
 
-  final mapController = MapController();
+  final mapControllerCompleter = Completer<YandexMapController>();
+
+  final List<MapObject> mapObjects = [];
+
+  Future<List> getCurrentLocation() async {
+    List listGeo = [];
+    await Geolocator.getCurrentPosition().then((value) {
+      setState(() {
+        listGeo = [value.latitude, value.longitude];
+      });
+    });
+    return listGeo;
+  }
+
+  void createMarker () async {
+    final MapObjectId mapObjectId = MapObjectId('normal_icon_placemark');
+
+    var list = await getCurrentLocation();
+
+    final mapObject = PlacemarkMapObject(
+      mapId: mapObjectId,
+      point: Point(latitude: list[0], longitude: list[1]),
+      onTap: (PlacemarkMapObject self, Point point) => print(mapObjectId),
+      opacity: 1,
+    );
+    
+    setState(() {
+      mapObjects.add(mapObject);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    createMarker();
+    _initPermission().ignore();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,47 +65,13 @@ class _Map extends State<Map> {
     return Scaffold(
       body: Stack(
           children: [
-            FlutterMap(
-              mapController: mapController,
-              options: MapOptions(
-                center: latLng.LatLng(47.237319946, 39.712245941),
-                zoom: 14.0,
-                maxZoom: 18.0,
-                minZoom: 5.0,
-              ),
-
-              children: [
-                TileLayer(
-                  urlTemplate:
-                      'https://api.mapbox.com/styles/v1/olardaniil/clf5o14q2000s01mrhe1byyg8/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib2xhcmRhbmlpbCIsImEiOiJjbGYyYW8zenkwMWk1M3JudG5jeW01ZHNoIn0.d5HskUsAcPfU479m_UAM0A',
-                  additionalOptions: {
-                    'accessToken': 'pk.eyJ1Ijoib2xhcmRhbmlpbCIsImEiOiJjbGYyYW8zenkwMWk1M3JudG5jeW01ZHNoIn0.d5HskUsAcPfU479m_UAM0A',
-                    'id': 'mapbox.mapbox-streets-v8',
-                  },
-                  subdomains: ['a', 'b', 'c'],
-                ),
-
-                new MarkerLayer(
-                  markers: [
-                    Marker(
-                      width: 45.0,
-                      height: 45.0,
-                      point: new latLng.LatLng(47.237319946, 39.712245941), 
-                      builder: (context) => new Container(
-                        child: IconButton(
-                          icon: Icon(Icons.location_on),
-                          color: Colors.red,
-                          iconSize: 45.0,
-                          onPressed: (){
-                            
-                          },
-                        ),
-                      )
-                    )
-                  ]
-                ),
-              ],
-            ),
+            YandexMap(
+            nightModeEnabled: true,
+            onMapCreated: (controller) {
+              mapControllerCompleter.complete(controller);
+            },
+            mapObjects: mapObjects,
+          ),
 
              Column(
                 children: [
@@ -184,6 +193,44 @@ class _Map extends State<Map> {
             // )
           ],
         )
+    );
+  }
+
+  /// Проверка разрешений на доступ к геопозиции пользователя
+  Future<void> _initPermission() async {
+    if (!await LocationService().checkPermission()) {
+      await LocationService().requestPermission();
+    }
+    await _fetchCurrentLocation();
+  }
+
+  /// Получение текущей геопозиции пользователя
+  Future<void> _fetchCurrentLocation() async {
+    AppLatLong location;
+    const defLocation = MoscowLocation();
+    try {
+      location = await LocationService().getCurrentLocation();
+    } catch (_) {
+      location = defLocation;
+    }
+    _moveToCurrentLocation(location);
+  }
+
+  /// Метод для показа текущей позиции
+  Future<void> _moveToCurrentLocation(
+    AppLatLong appLatLong,
+  ) async {
+    (await mapControllerCompleter.future).moveCamera(
+      animation: const MapAnimation(type: MapAnimationType.linear, duration: 1),
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: Point(
+            latitude: appLatLong.lat,
+            longitude: appLatLong.long,
+          ),
+          zoom: 12,
+        ),
+      ),
     );
   }
 }
