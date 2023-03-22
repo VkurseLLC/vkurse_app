5663993b04369821bc8eb4c32a5ec41e7b4518f9
THKsss <pavukdmitriy@gmail.com>
2023-03-22 21:45:47 +0300

    Промежуточный
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   assets/images/danilLogo.jpg
    	new file:   assets/images/dmitriyLogo.jpg
    	new file:   assets/images/nikitaLogo.jpg
    	new file:   assets/images/semenLogo.jpg
    	modified:   lib/pages/map.dart
    	modified:   lib/pages/testDmitriy.dart
    	modified:   lib/ui/main_navigation.dart
    	modified:   pubspec.lock
    	modified:   pubspec.yaml

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,16 +1,19 @@
-import 'dart:ui';
-import 'dart:async';
-import 'dart:developer';
-import 'dart:typed_data';
+// import 'dart:ui';
+// import 'dart:async';
+// import 'dart:developer';
+// import 'dart:typed_data';
 
 import 'package:cron/cron.dart';
-import 'package:dgis_flutter/dgis_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:latlong2/latlong.dart' as latLng;
+import 'package:auto_size_text/auto_size_text.dart';
 
 import 'package:flutter/material.dart';
 import 'package:vkurse_app/data/api_location.dart';
 import 'package:vkurse_app/pages/app_location.dart';
-import 'package:vkurse_app/pages/assets_const.dart';
 
 
 void locationHandler () {
@@ -31,64 +34,146 @@ class Map extends StatefulWidget {
 }
 
 class _Map extends State<Map> {
-  final GisMapController controller = GisMapController();
-
-  late final Future<List<GisMapMarker>> icons;
-  List<GisMapMarker> list = [];
-  List<GisMapMarker> mapObject = [];
   
-  void createMarker(_user_id, _latitude, _longitude, _context) async {
-    var iconMarker = await getPngFromAsset(_context, AssetPath.iconsPointGrey, 120);
-    mapObject.add(GisMapMarker(icon: iconMarker, latitude: _latitude, longitude: _longitude, zIndex: 0, id: _user_id.toString()));
-    print("mapObject: $mapObject");
-  }
+  final mapController = MapController();
 
-  void placeMarker(_context) {
-  
-    final cron = Cron();
-    cron.schedule(Schedule.parse('*/5 * * * * *'), () async {
+  List<Marker> markers = [];
 
-    // var userLocation = [{"type": "user_location", "user_id": "2", "latitude": 47.289020, "longitude": 39.702150}, {"type": "user_location", "user_id": "3", "latitude": 47.289020, "longitude": 39.701150} ];
-      var userLocation = await LocationApi.users_location_stream("2");
-      var data_item = null;
+  List userInfo = [["Kratos0506", "assets/images/nikitaLogo.jpg", 47.237339, 39.712246], ["Semyown", "assets/images/semenLogo.jpg", 47.239339, 39.712246], ["olardaniil", null, 47.637339, 39.715246], ["THKssssssssssss", null, 47.639339, 39.715246]];
 
-      for (data_item in userLocation) {
-        if (data_item["type"] == "user_location" || data_item["type"] == "friend_location") {
-          createMarker(data_item["user_id"], data_item["latitude"], data_item["longitude"], _context);
+  void createMarker(_user_id, _photo, _latitude, _longitude) {
+    for(var info in userInfo){
+      if(info[1] != null){
+        var mark = Marker(
+          width: 90.0,
+          height: 90.0,
+          point: latLng.LatLng(info[2], info[3]),
+          builder: (ctx) => IconButton(
+            onPressed: (){},
+            icon: Container(
+              child: Stack(
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Positioned(
+                        child: SizedBox(
+                          width: 90,
+                          height: 90,
+                          child: CustomPaint(
+                            foregroundPainter: IconPainter(),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        // top: 8,
+                        // left: width * 0.325,
+                        child: Container(
+                          height: 48,
+                          width: 48,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            image: DecorationImage(
+                              image: AssetImage(info[1]),
+                            // opacity: 0.4,
+                            )
+                          )
+                        )
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          )
+        );  
+        setState(() {
+          markers.add(mark);
+        });
+          
+        } else {
+          var mark = Marker(
+            width: 90.0,
+            height: 90.0,
+            point: latLng.LatLng(info[2], info[3]),
+            builder: (ctx) => IconButton(
+              onPressed: (){},
+              icon: Container(
+                child: Stack(
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Positioned(
+                          child: SizedBox(
+                            width: 90,
+                            height: 90,
+                            child: CustomPaint(
+                              foregroundPainter: IconPainter(),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                        // top: 8,
+                        // left: width * 0.325,
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            alignment: Alignment.center,
+                            child: AutoSizeText(
+                              info[0],
+                              maxLines: 1,
+                              minFontSize: 10,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontFamily: "Comfortaa",
+                              ),
+                            ),
+                          )
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            )
+          );
+          setState(() {
+            markers.add(mark);
+          });
         }
       }
-
-      final status = controller.updateMarkers(mapObject as List<GisMapMarker>);
-      mapObject.clear();
-    });
   }
 
   @override
   void initState() {
-
-    placeMarker(context);
-    locationHandler();
-    _initPermission();
-
-    icons = Future.wait([getPngFromAsset(context, AssetPath.iconsPointGrey, 60)]).then(
-        (value) => [GisMapMarker(icon: value[0], latitude: 47.289020, longitude: 39.701150, zIndex: 0, id: "test")]);
-    super.initState();
+    createMarker(userInfo[0][0], userInfo[0][1], userInfo[0][2], userInfo[0][3]);
+    // placeMarker(context);
+    // locationHandler();
+    // _initPermission();
   }
 
-  Future<Uint8List> getPngFromAsset(
-    BuildContext context,
-    String path,
-    int width,
-  ) async {
-    ByteData data = await DefaultAssetBundle.of(context).load(path);
-    Codec codec = await instantiateImageCodec(
-      data.buffer.asUint8List(),
-      targetWidth: width,
-    );
-    FrameInfo fi = await codec.getNextFrame();
-    return (await fi.image.toByteData(format: ImageByteFormat.png))!.buffer.asUint8List();
-  }
+  // void placeMarker(_context) {
+  
+  //   final cron = Cron();
+  //   cron.schedule(Schedule.parse('*/5 * * * * *'), () async {
 
+  //   // var userLocation = [{"type": "user_location", "user_id": "2", "latitude": 47.289020, "longitude": 39.702150}, {"type": "user_location", "user_id": "3", "latitude": 47.289020, "longitude": 39.701150} ];
+  //     var userLocation = await LocationApi.users_location_stream("2");
+  //     var data_item = null;
+
+  //     for (data_item in userLocation) {
+  //       if (data_item["type"] == "user_location" || data_item["type"] == "friend_location") {
+  //         createMarker(data_item["user_id"], data_item["latitude"], data_item["longitude"], _context);
+  //       }
+  //     }
+
+  //     final status = controller.updateMarkers(mapObject as List<GisMapMarker>);
+  //     mapObject.clear();
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -99,35 +184,53 @@ class _Map extends State<Map> {
 
     return Scaffold(
       body: Stack(
-          // ignore: sort_child_properties_last
           children: [
-            SizedBox(
-              width: width,
-              height: height,
-              child: FutureBuilder<List<GisMapMarker>>(
-                future: icons,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) return const SizedBox();
-                  list = snapshot.data!;
-                  return GisMap(
-                    directoryKey: 'runhin9102',
-                    mapKey: '9d6ae96d-d730-42f7-a6a6-2e378f46215a',
-                    // directoryKey: 'rubyqf9316',
-                    // mapKey: 'b7272230-6bc3-47e9-b24b-0eba73b12fe1',
-                    useHybridComposition: true,
-                    controller: controller,
-                    onTapMarker: (marker) {
-                      
-                    },
-                    startCameraPosition: const GisCameraPosition(
-                      latitude: 47.237319946,
-                      longitude: 39.712245941,
-                      tilt: 25.0,
-                      zoom: 14.0,
-                    ),
-                  );
-                },
+            FlutterMap(
+              mapController: mapController,
+              options: MapOptions(
+                center: latLng.LatLng(47.237319946, 39.712245941),
+                zoom: 13.0,
+                maxZoom: 18.0,
+                minZoom: 3.0,
               ),
+
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      'https://api.mapbox.com/styles/v1/olardaniil/clf5o14q2000s01mrhe1byyg8/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib2xhcmRhbmlpbCIsImEiOiJjbGZpbW9nM2MxczdtM3RuejV5OGxkeHMxIn0.vj2t-Rt79XjS6N225mhoRw',
+                  additionalOptions: {
+                    'accessToken': 'pk.eyJ1Ijoib2xhcmRhbmlpbCIsImEiOiJjbGZpbW9nM2MxczdtM3RuejV5OGxkeHMxIn0.vj2t-Rt79XjS6N225mhoRw',
+                    'id': 'mapbox.mapbox-streets-v8',
+                  },
+                ),
+
+                MarkerClusterLayerWidget(
+                  options: MarkerClusterLayerOptions(
+                    maxClusterRadius: 40,
+                    disableClusteringAtZoom: 10,
+                    size: Size(50, 50),
+                    // anchor: AnchorPos.align(AnchorAlign.center),
+                    rotate: true,
+                    rotateAlignment: Alignment.center,
+                    markers: markers,
+                    builder: (context, markers) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(),
+                            color: Color(0xFF894EB8)
+                        ),
+                        child: Center(
+                          child: Text(
+                            markers.length.toString(),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
 
             Positioned(
@@ -154,16 +257,7 @@ class _Map extends State<Map> {
                 width: width * 0.1216, //50
                 height: width * 0.1216, //50
                 child: ElevatedButton(
-                  onPressed: () async {
-                    // Position geo = await Geolocator.getCurrentPosition();
-                    // final status = await controller.setCameraPosition(latitude: geo.latitude, longitude: geo.longitude, zoom: 16);
-                    final status = await controller.setCameraPosition(
-                      latitude: 47.289020, 
-                      longitude: 39.702150,
-                      zoom: 16
-                    );
-                    log(status);
-                  }, 
+                  onPressed: () {}, 
                   child: Image.asset("assets/icons/currentPosition.png"),
                   style: ButtonStyle(
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
@@ -255,33 +349,89 @@ class _Map extends State<Map> {
     }
 
   // / Проверка разрешений на доступ к геопозиции пользователя
-  Future<void> _initPermission() async {
-    if (!await LocationService().checkPermission()) {
-      await LocationService().requestPermission();
-    }
-    await _fetchCurrentLocation();
+  // Future<void> _initPermission() async {
+  //   if (!await LocationService().checkPermission()) {
+  //     await LocationService().requestPermission();
+  //   }
+  //   await _fetchCurrentLocation();
+  // }
+
+  // /// Получение текущей геопозиции пользователя
+  // Future<void> _fetchCurrentLocation() async {
+  //   AppLatLong location;
+  //   const defLocation = MoscowLocation();
+  //   try {
+  //     location = await LocationService().getCurrentLocation();
+  //   } catch (_) {
+  //     location = defLocation;
+  //   }
+  //   _moveToCurrentLocation(location);
+  // }
+
+  // /// Метод для показа текущей позиции
+  // Future<void> _moveToCurrentLocation(
+  //   AppLatLong appLatLong,
+  // ) async {
+  //   (await mapcontroller.setCameraPosition(
+  //     latitude: appLatLong.lat,
+  //     longitude: appLatLong.long,
+  //     zoom: 10,
+  //   ));
+  // }
+}
+
+class IconPainter extends CustomPainter{
+  
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final sw = size.width;
+    final sh = size.height;
+
+    Paint paint0 = Paint()
+      ..color = Color(0xFF894EB8)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1;
+     
+         
+    Path path0 = Path();
+    path0.moveTo(sw * 0.3, sh * 0.8);
+    path0.lineTo(sw * 0.7, sh * 0.8);
+    path0.quadraticBezierTo(sw * 0.5409, sh * 0.9994, sw * 0.5, sh);
+    path0.quadraticBezierTo(sw * 0.4591, sh * 1.0007, sw * 0.3, sh * 0.8);
+    path0.close();
+
+    canvas.drawPath(path0, paint0);
+  
+
+  Paint paint1 = Paint()
+      ..color = Color(0xFF894EB8)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1;
+
+    Path path1 = Path();
+    path1.moveTo(sw * 0.1, sh * 0.3);
+    path1.quadraticBezierTo(sw * 0.1013, sh * 0.1013, sw * 0.3, sh * 0.1);
+
+    path1.lineTo(sw * 0.7,sh * 0.1);
+    path1.quadraticBezierTo(sw * 0.9001, sh * 0.1012,sw * 0.9, sh * 0.3);
+    path1.cubicTo(sw * 0.9, sh * 0.2125, sw * 0.9, sh * 0.7875, sw * 0.9, sh * 0.7);
+    path1.quadraticBezierTo(sw * 0.9, sh * 0.9, sw * 0.7, sh * 0.9);
+
+    path1.lineTo(sw * 0.3, sh * 0.9);
+    path1.quadraticBezierTo(sw * 0.1010, sh * 0.9, sw * 0.1, sh * 0.7);
+    path1.cubicTo(sw * 0.1, sh * 0.7875, sw * 0.1, sh * 0.2375, sw * 0.1, sh * 0.3);
+
+    path1.close();
+
+    canvas.drawPath(path1, paint1);
+  
+    
   }
 
-  /// Получение текущей геопозиции пользователя
-  Future<void> _fetchCurrentLocation() async {
-    AppLatLong location;
-    const defLocation = MoscowLocation();
-    try {
-      location = await LocationService().getCurrentLocation();
-    } catch (_) {
-      location = defLocation;
-    }
-    _moveToCurrentLocation(location);
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
-
-  /// Метод для показа текущей позиции
-  Future<void> _moveToCurrentLocation(
-    AppLatLong appLatLong,
-  ) async {
-    (await controller.setCameraPosition(
-      latitude: appLatLong.lat,
-      longitude: appLatLong.long,
-      zoom: 10,
-    ));
-  }
+  
 }
