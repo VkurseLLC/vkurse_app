23d1ab459f8ba30a7b330f8f99dd71295b8c6e30
THKsss <pavukdmitriy@gmail.com>
2023-03-26 18:33:36 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    Редизайн процесса регистрации + логистика по папкам
    
    Changes to be committed:
    	modified:   lib/pages/auth_get_verification_code.dart
    
    Changes not staged for commit:
    	modified:   lib/custom_package/src/widgets/item.dart
    	modified:   lib/main.dart
    	deleted:    lib/pages/app_location.dart
    	deleted:    lib/pages/assets_const.dart
    	modified:   lib/pages/auth_phone_number.dart
    	deleted:    lib/pages/auth_verification_code copy.dart
    	modified:   lib/pages/auth_verification_code.dart
    	modified:   lib/pages/end_profile_filling.dart
    	modified:   lib/pages/input_username.dart
    	modified:   lib/pages/loading.dart
    	modified:   lib/pages/map.dart
    	modified:   lib/pages/profile.dart
    	modified:   lib/pages/profile_filling.dart
    	modified:   lib/pages/welcome.dart
    	modified:   lib/ui/main_navigation.dart
    	modified:   linux/flutter/generated_plugin_registrant.cc
    	modified:   linux/flutter/generated_plugin_registrant.h
    	modified:   linux/flutter/generated_plugins.cmake
    	modified:   macos/Flutter/GeneratedPluginRegistrant.swift
    	modified:   windows/flutter/generated_plugin_registrant.cc
    	modified:   windows/flutter/generated_plugin_registrant.h
    	modified:   windows/flutter/generated_plugins.cmake
    
    Untracked files:
    	.vscode/
    	lib/pages/additionally/
    	lib/pages/style/

--- lib/pages/auth_get_verification_code.dart
+++ lib/pages/auth_get_verification_code.dart
@@ -1,20 +1,23 @@
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/foundation.dart';
+//_____________________________________________СИСТЕМНЫЕ________________________________________________\\
+
 import 'package:flutter/material.dart';
+
+//_____________________________________________БИБЛИОТЕКИ________________________________________________\\
+
 import 'package:url_launcher/link.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:vkurse_app/pages/auth_verification_code.dart';
-import '../generated/locale_keys.g.dart';
+import 'package:flutter/foundation.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:easy_localization/easy_localization.dart';
 
-// Класс для отображения страницы перехода к боту
-final _url = Uri.parse("https://t.me/VkurseAppBot");
+//_____________________________________________ДРУГИЕ ФАЙЛЫ________________________________________________\\
 
-void _launchUrl(_url)async {
-  if (await launchUrl(_url)) {
-    throw Exception('Could not launch $_url');
-  }
-}
+import '../generated/locale_keys.g.dart';
+import 'package:vkurse_app/pages/style/canvas.dart';
+import 'package:vkurse_app/pages/auth_verification_code.dart';
+
+
+
+// Класс для отображения страницы перехода к боту
 
 class AuthGetVerificationCode extends StatefulWidget {
   const AuthGetVerificationCode({Key? key}) : super(key: key);
@@ -25,267 +28,213 @@ class AuthGetVerificationCode extends StatefulWidget {
 }
 
 class _AuthGetVerificationCodeState extends State<AuthGetVerificationCode> {
-  // bool isButtonActive = false;
 
   @override
   Widget build(BuildContext context) {
+
     final mediaQuery = MediaQuery.of(context);
-    var width = mediaQuery.size.width;
-    var height = mediaQuery.size.height;
-    var buttonWidth = width * 0.65;
-    var buttonHeight = buttonWidth * 0.20;
-
-    var widthText = width * 0.85;
-    var heightText = widthText * 0.2;
-    double fontSizeButton = 16.0;
-    double fontSizeText = 20.0;
-
-    if(width <= 300)
-    {
-        fontSizeText = 22.0;
-        fontSizeButton = 16.0;
-
-    }
-    else if (width > 300 && width <= 700) 
-    {
-        fontSizeText = 30.0;
-        fontSizeButton = 20.0;
-
-    }
-    else if (width > 700 && width <= 1000) 
-    {
-        fontSizeText = 35.0;
-        fontSizeButton = 30.0;
-        widthText = width * 0.65;
-        heightText = widthText * 0.2;
-        buttonHeight = buttonWidth * 0.15;
-    }
-    else if (width > 1000) 
-    {
-        fontSizeText = 40.0;
-        fontSizeButton = 30.0;
-        widthText = width * 0.65;
-        heightText = widthText * 0.2;
-        buttonHeight = buttonWidth * 0.15;
-    }
+    var width = mediaQuery.size.width, height = mediaQuery.size.height;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-
-             ///\\\ (НАЧАЛО) ///\\\ ИКОНКА "НАЗАД" + ЛОГО ///\\\
-            
-            Padding(padding: EdgeInsets.fromLTRB(0, width * 0.05, 0, 0)),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-
-                Padding(padding: EdgeInsets.fromLTRB(width * 0.05, 0, 0, 0)),
-
-                Column(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: width,
+                height: height * 0.3,
+                child: Stack(
+                  alignment: Alignment.center,
                   children: [
-                    Container(
-                      width: width * 0.2,
-                      height: width * 0.2,
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0x26A0A0A0),
-                            blurRadius: 20,
-                          )
-                        ]
+                    Positioned(
+                      child: SizedBox(
+                        width: width,
+                        height: height * 0.3,
+                        child: CustomPaint(
+                          foregroundPainter: HeaderPainter(),
+                        ),
                       ),
-                      child: IconButton(
-                        onPressed: () async {
-                          await Navigator.pushNamed(context, '/auth_get_phone_number');
-                        }, 
-                        icon: const Icon(Icons.arrow_back_ios_new),
-                        iconSize: width * 0.1,
-                        color: Color(0xFFA8A8A8),
+                    ),
+
+                    Positioned(
+                      left: width * 0.025,
+                      child: SizedBox(
+                        width: width * 0.2,
+                        height: width * 0.2,
+                        child: IconButton(
+                          onPressed: () async {
+                            await Navigator.pushNamed(context, '/auth');
+                          }, 
+                          icon: const Icon(Icons.arrow_back_ios_new),
+                          iconSize: width * 0.1,
+                          color: Color(0xF0DADADA),
+                        )
+                      )
+                    ),
+
+                    Positioned(
+                      child: Container(
+                        height: width * 0.4,
+                        width: width * 0.4,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/logo_pic_white.png"),
+                          opacity: 0.4,
+                          )
+                        ),
                       )
                     )
                   ],
                 ),
+              ),
+            ],
+          ),
 
+          Expanded(
+            child: Row(
+              children: [
                 Expanded(
                   child: Column(
                     children: [
-                        Container(
-                        height: width * 0.35,
-                        width: width * 0.35,
-                        decoration: BoxDecoration(
-                            image: const DecorationImage(
-                              image: AssetImage("assets/images/logo_pic.png"),
-                            opacity: 0.4,
-                            )
-                          ),
-                        )
-                    ],
-                  )
-                ),
-                
-                Padding(padding: EdgeInsets.fromLTRB(0, 0, width * 0.2 + width * 0.05, 0)),
-                
-              ],
-            ),
-
-             //!\\\ (КОНЕЦ) ///\\\ ИКОНКА "НАЗАД" + ЛОГО ///\\\
-            
-            //---------------------------------------------------------------------------------------------------------------------------------------\\
-
-            ///\\\ (НАЧАЛО) ///\\\ ЛОГО ///\\\
-
-            SizedBox(
-              width: width * 0.2,
-              height: width * 0.2,
-            ),
-
-            //!\\\ (КОНЕЦ) ///\\\ ЛОГО ///\\\
-            
-            //---------------------------------------------------------------------------------------------------------------------------------------\\
-
-            ///\\\ (НАЧАЛО) ///\\\ ПЕРЕХОД К БОТУ (ТЕКСТ + КНОПКА) ///\\\
-
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        width: widthText,
-                        height: heightText,
-                        child: AutoSizeText(
-                          "Для продолжения Вам необходимо получить код подтверждения в нашем Телеграмм-боте",
-                          style: TextStyle(
-                            color: Color(0xFF6F2EAE),
-                            fontSize: fontSizeText,
-                            fontFamily: "assets/fonts/Inter-Bold.ttf",
-                            fontWeight: FontWeight.bold
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              width: width * 0.8718,
+                              height: width * 0.2583,
+                              child: const AutoSizeText(
+                                "Для продолжения Вам необходимо получить код подтверждения в нашем Телеграмм-боте",
+                                maxLines: 5,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Color(0xF0894EB8),
+                                  fontSize: 40,
+                                  fontFamily: "Comfortaa",
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ), 
                             ),
-                          textAlign: TextAlign.center,
-                          maxLines: 5,
-                        ),),
-
-                      Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, width * 0.05)),
-                      
-
-                      Container(
-                        width: buttonWidth,
-                        height: buttonHeight,
-
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Color.fromARGB(70, 0, 0, 0),
-                              blurRadius: 25,
-                              offset: Offset(0.0, 10.0)),
                           ],
-                        ),
+                        )
+                      ),
 
-                        child: Opacity(
-                          opacity: 0.9,
-                            child: Link(
-                              target: LinkTarget.blank,
-                              uri: Uri.parse("https://t.me/VkurseAppBot"),
-                              builder: (context, followLink) => ElevatedButton(            
-                                onPressed: followLink,
-                                style: ButtonStyle(
-                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(width * 0.039),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: width * 0.6538,
+                              height: width * 0.1333,
+
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color.fromARGB(70, 0, 0, 0),
+                                    blurRadius: width * 0.0608,
+                                    offset: Offset(0.0, width * 0.0243)),
+                                ],
+                              ),
+
+                              child: Opacity(
+                                opacity: 0.9,
+                                  child: Link(
+                                    target: LinkTarget.blank,
+                                    uri: Uri.parse("https://t.me/VkurseAppBot"),
+                                    builder: (context, followLink) => ElevatedButton(            
+                                      onPressed: followLink,
+                                      style: ButtonStyle(
+                                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(width * 0.039),
+                                          ),
+                                        ),
+                                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xE6350068)),
+                                      ),
+
+                                      child: SizedBox(
+                                        width: width * 0.4385,
+                                        height: width * 0.0564,
+                                        child: const AutoSizeText(
+                                          'Телеграмм-бот',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: 60,
+                                            fontFamily: "Comfortaa",
+                                            fontWeight: FontWeight.bold
+                                          ),
+                                        ),
+                                      )
                                     ),
                                   ),
-                                  backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF350068)),
-                                ),
-                                child: AutoSizeText(
-                                  'Телеграм-бот',
-                                  style: TextStyle(
-                                    fontSize: fontSizeButton,
-                                    fontFamily: "assets/fonts/Inter-Regular.ttf",
-                                    fontWeight: FontWeight.bold),
-                                ),
                               ),
                             ),
-                        ),
+                          ],
+                        )
                       ),
                     ],
                   )
-                ],
-              )
-            ),
+                )
+              ],
+            )
+          ),
 
-            //!\\\ (КОНЕЦ) ///\\\ ПЕРЕХОД К БОТУ (ТЕКСТ + КНОПКА) ///\\\
-            
-            //---------------------------------------------------------------------------------------------------------------------------------------\\
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: width * 0.6538,
+                  height: width * 0.1333,
+                  
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(70, 0, 0, 0),
+                        blurRadius: width * 0.0608,
+                        offset: Offset(0.0, width * 0.0243)),
+                    ],
+                  ),
 
-            ///\\\ (НАЧАЛО) ///\\\ КНОПКА "ПРОДОЛЖИТЬ" ///\\\
+                  child: Opacity(
+                    opacity: 0.9,
+                    child: ElevatedButton(
+                      
+                      onPressed:() async {
+                        await Navigator.pushNamed(context, '/auth_post_verification_code');
+                      },
 
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: buttonWidth,
-                        height: buttonHeight,
-                        
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Color.fromARGB(70, 0, 0, 0),
-                              blurRadius: 25,
-                              offset: Offset(0.0, 10.0)),
-                          ],
-                        ),
-
-                        child: Opacity(
-                          opacity: 0.9,
-                          child: ElevatedButton(
-                          onPressed:() async {
-                            await Navigator.pushNamed(context, '/auth_post_verification_code');
-                          },
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(width * 0.039),
-                                ),
-                              ),
-                            backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF6F2EAE))
-                            ),
-
-                            child:
-                            AutoSizeText(
-                                'Продолжить',
-                                style: TextStyle(
-                                  fontSize: fontSizeButton,
-                                  fontFamily: "assets/fonts/Inter-Regular.ttf",
-                                  fontWeight: FontWeight.bold),
-                            )
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                           RoundedRectangleBorder(
+                             borderRadius: BorderRadius.circular(width * 0.039),
+                           ),
+                         ),
+                         backgroundColor: MaterialStateProperty.all<Color>(Color(0xF0894EB8))                  
+                      ),
+                      child: SizedBox(
+                        width: width * 0.3692,
+                        height: width * 0.0564,
+                        child: const AutoSizeText(
+                          'Продолжить',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontFamily: "Comfortaa",
+                            fontWeight: FontWeight.bold
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: width * 0.25,
                       )
-                    ],
-                  )
-                ],
-              )
-            ),
-            //!\\\ (КОНЕЦ) ///\\\ КНОПКА "ПРОДОЛЖИТЬ" ///\\\
-
-          ]
-        ),
+                    ),
+                  ),
+                ),
+              ],
+            ) 
+          )
+        ],
       ),
     );
   }
